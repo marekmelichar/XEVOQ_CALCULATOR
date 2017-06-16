1ed06de7932fd4d5c2ca5594ea4911faf9de07cb
Marek Melichar <koprivackej@gmail.com>
2017-06-16 19:45:49 +0200

    looks like left side works

--- app/src/containers/outputs/Outputs.js
+++ app/src/containers/outputs/Outputs.js
@@ -10,6 +10,10 @@ class Outputs extends Component {
 
     let { logic } = nextProps
 
+    if (logic.number_of_rooms) {
+      this.setState({ number_of_rooms: logic.number_of_rooms })
+    }
+
     if (logic.total_square_meters > 0) {
       this.setState({ totalSquareMeters: parseFloat(logic.total_square_meters) })
     } else {
@@ -34,39 +38,73 @@ class Outputs extends Component {
       this.setState({ averageSalary: 650 })
     }
 
+    if(logic.number_of_rooms) {
+      this.setState({ F39: ((this.state.totalSquareMeters * this.state.priceOfRent1sqm) / (20 * 8) / parseFloat(logic.number_of_rooms)).toFixed(1) })
+    }
+
+    if (logic.amount_of_meetings_daily > 0) {
+
+      let J20 = (30 / 60) * (logic.amount_of_meetings_daily * 0.25) * this.state.F39
+      let J28 = (15 / 60) * (logic.amount_of_meetings_daily * 0.225) * (this.state.average_occupancy_of_rooms * this.state.averageSalary)
+
+      let a = J20 + J28
+      let b = Math.ceil(a / 100.0) * 100
+
+      this.setState({ final_savings_daily: b })
+    } else {
+
+      let F18 = ((30 / 60) * (parseFloat(this.state.number_of_rooms) * this.state.F39))
+      let F28 = ((15 / 60) * (this.state.average_occupancy_of_rooms * this.state.averageSalary) * logic.number_of_rooms * 0.9)
+
+      let a = F18 + F28
+      let b = Math.ceil(a / 100.0) * 100
+
+      this.setState({ final_savings_daily: b })
+
+      let F21 = F18 * 4 * 4
+      let F31 = F28 * 4 * 4
+
+      let c = F21 + F31
+      let d = Math.ceil(c / 1000.0) * 1000
+
+      this.setState({ final_savings_monthly: d })
+    }
   }
 
   constructor(props) {
     super(props)
 
     this.state = {
-      returnOfInvestment: 0,
-      betterOrganizedMeetingsSavings: 0,
-      increasedCapacity: 0,
-      savings: 0,
-      priceOfRent1sqm: 500,
+      // returnOfInvestment: 0,
+      // betterOrganizedMeetingsSavings: 0,
+      // increasedCapacity: 0,
+      // savings: 0,
+      number_of_rooms: 0,
+      average_occupancy_of_rooms: 3.6,
+      amount_of_meetings_daily: 0,
       totalSquareMeters: 165,
+      priceOfRent1sqm: 500,
       averageSalary: 650,
       final_savings_daily: 0,
       final_savings_monthly: 0,
-      average_occupancy_of_rooms: 3.6,
-
-      B31: 10,
-      B33: 12,
-      B35: 3,
-      B23: 15,
-      B25: 3,
-      B27: 6,
-      B16: 500,
-      B19: 650,
-      B42: 0,
-      C8: 0,
-      J18: 0.25,
-      J19: 0,
-      F35: 0, // 165 m2
-      F37: 0, // 500 kc / m2
-      C3: 0,
       F39: 0
+
+
+      // B31: 10,
+      // B33: 12,
+      // B35: 3,
+      // B23: 15,
+      // B25: 3,
+      // B27: 6,
+      // B16: 500,
+      // B19: 650,
+      // B42: 0,
+      //C8: 0,
+      // J18: 0.25,
+      // J19: 0,
+      //F35: 0, // 165 m2
+      //F37: 0, // 500 kc / m2
+      // C3: 0,
     }
   }
 
@@ -94,17 +132,6 @@ class Outputs extends Component {
 
     return (
       <div>
-        <div id="price-of-rent-1sqm">
-          <label htmlFor="_price-of-rent-1sqm"><h3>Price of rent / 1 square meter:</h3></label>
-          <input
-            id="_price-of-rent-1sqm"
-            type="number"
-            value="0"
-            onChange={event => this.priceOfRent1sqm(event)}
-            value={this.state.priceOfRent1sqm}
-            onFocus={event => this.handleFocus(event)}
-          />
-        </div>
         <div id="total-square-meters">
           <label htmlFor="_total-square-meters"><h3>Total square meters of all meeting rooms:</h3></label>
           <input
@@ -113,6 +140,17 @@ class Outputs extends Component {
             value="0"
             onChange={event => this.totalSquareMeters(event)}
             value={this.state.totalSquareMeters}
+            onFocus={event => this.handleFocus(event)}
+          />
+        </div>
+        <div id="price-of-rent-1sqm">
+          <label htmlFor="_price-of-rent-1sqm"><h3>Price of rent / 1 square meter:</h3></label>
+          <input
+            id="_price-of-rent-1sqm"
+            type="number"
+            value="0"
+            onChange={event => this.priceOfRent1sqm(event)}
+            value={this.state.priceOfRent1sqm}
             onFocus={event => this.handleFocus(event)}
           />
         </div>
@@ -128,38 +166,39 @@ class Outputs extends Component {
           />
         </div>
 
-        <div id="return-of-investment">
+
+        {/* <div id="return-of-investment">
           <label htmlFor="_return-of-investment"><h3>Return on investment / days:</h3></label>
           <input
             id="_return-of-investment"
             type="text"
             value={this.state.returnOfInvestment}
           />
-        </div>
-        <div id="better-organized-meetings-savings">
+        </div> */}
+        {/* <div id="better-organized-meetings-savings">
           <label htmlFor="_better-organized-meetings-savings"><h3>Yearly savings based on saved hours thanks to better organized meetings:</h3></label>
           <input
             id="_better-organized-meetings-savings"
             type="text"
             value={this.state.betterOrganizedMeetingsSavings}
           />
-        </div>
-        <div id="increased-capacity-of-meetings">
+        </div> */}
+        {/* <div id="increased-capacity-of-meetings">
           <label htmlFor="_increased-capacity-of-meetings"><h3>Increased capacity in % thanks to Cancel function and automated cancelling of meetings:</h3></label>
           <input
             id="_increased-capacity-of-meetings"
             type="text"
             value={this.state.increasedCapacity}
           />
-        </div>
-        <div id="savings-in-czk">
+        </div> */}
+        {/* <div id="savings-in-czk">
           <label htmlFor="_savings-in-czk"><h3>Savings in CZK for increasing the capacity of meeting rooms:</h3></label>
           <input
             id="_savings-in-czk"
             type="text"
             value={this.state.savings}
           />
-        </div>
+        </div> */}
         <div id="FINAL_SAVINGS_DAILY">
           <label htmlFor="_FINAL_SAVINGS_DAILY"><h3>Final Savings in CZK DAILY:</h3></label>
           <input
